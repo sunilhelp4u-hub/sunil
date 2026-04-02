a31daef570ec4ef14d8cbf5d0521d1acc84f6db2
sunilhelp4u-hub <sunilhelp4u@gmail.com>
2026-04-02 13:40:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ CLIENT_ID = "your_id"
 PASSWORD = "your_pass"
 TOTP_SECRET = "your_totp"
 
-TELEGRAM_TOKEN = "your_bot_token"
+TELEGRAM_TOKEN = "8331918470:AAEpnz6rgY-AC3P6NuKyyeGiV06q0282YbQ"
 CHAT_ID = "your_chat_id"
 
 BOT_RUNNING = True
